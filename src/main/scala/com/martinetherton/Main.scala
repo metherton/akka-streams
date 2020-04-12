c152609b9294ad2f65e6a1bddf8b5c51a58ba6c9
metherton <martinetherton63@gmail.com>
2020-04-12 21:25:27 +0200

    wiring up different parts of a stream

--- src/main/scala/com/martinetherton/Main.scala
+++ src/main/scala/com/martinetherton/Main.scala
@@ -1,8 +1,9 @@
 package com.martinetherton
 
+import akka.NotUsed
 import akka.actor.ActorSystem
 import akka.stream.ActorMaterializer
-import akka.stream.scaladsl.{Keep, RunnableGraph, Sink, Source}
+import akka.stream.scaladsl.{Flow, Keep, RunnableGraph, Sink, Source}
 
 import scala.concurrent.Future
 
@@ -13,29 +14,19 @@ object Main extends App {
   // needed for the future flatMap/onComplete in the end
   implicit val executionContext = system.dispatcher
 
-  // Create a source from an Iterable
-  Source(List(1, 2, 3))
+  // Explicitly creating and wiring up a Source, Sink and Flow
+  Source(1 to 6).via(Flow[Int].map(_ * 2)).to(Sink.foreach(println(_)))
 
-  // Create a source from a Future
-  Source.fromFuture(Future.successful("Hello Streams!"))
+  // Starting from a Source
+  val source = Source(1 to 6).map(_ * 2)
+  source.to(Sink.foreach(println(_)))
 
-  // Create a source from a single element
-  Source.single("only one element")
+  // Starting from a Sink
+  val sink: Sink[Int, NotUsed] = Flow[Int].map(_ * 2).to(Sink.foreach(println(_)))
+  Source(1 to 6).to(sink)
 
-  // an empty source
-  Source.empty
-
-  // Sink that folds over the stream and returns a Future
-  // of the final result as its materialized value
-  Sink.fold[Int, Int](0)(_ + _)
-
-  // Sink that returns a Future as its materialized value,
-  // containing the first element of the stream
-  Sink.head
-
-  // A Sink that consumes a stream without doing anything with the elements
-  Sink.ignore
-
-  // A Sink that executes a side-effecting call for every element of the stream
-  Sink.foreach[String](println(_))
+  // Broadcast to a sink inline
+  val otherSink: Sink[Int, NotUsed] =
+    Flow[Int].alsoTo(Sink.foreach(println(_))).to(Sink.ignore)
+  Source(1 to 6).to(otherSink)
 }
