3337d29b4c6f65d21d7afb8304208c7ab7397fde
metherton <martinetherton63@gmail.com>
2020-04-13 12:01:44 +0200

    read delimited lines

--- src/main/scala/com/martinetherton/Main.scala
+++ src/main/scala/com/martinetherton/Main.scala
@@ -4,7 +4,7 @@ import java.nio.file.Paths
 
 import akka.NotUsed
 import akka.actor.ActorSystem
-import akka.stream.ActorMaterializer
+import akka.stream.{ActorMaterializer, IOResult}
 import akka.stream.scaladsl.{FileIO, Flow, Framing, Keep, RunnableGraph, Sink, Source}
 import akka.util.ByteString
 
@@ -17,16 +17,23 @@ object Main extends App {
   // needed for the future flatMap/onComplete in the end
   implicit val executionContext = system.dispatcher
 
-  val source = Source(1 to 10)
-  val doubles = (x: Int) => x * 2
-  val doubleSource = source.map(doubles)
-  val sink = Sink.fold[Int, Int](0)(_ + _)
+  val file = Paths.get("test.csv")
+  val res: Source[ByteString, Future[IOResult]] = FileIO.fromPath(file)
+  val f = res.via(Framing.delimiter(ByteString("\n"), 256, true)).map(_.utf8String)
+  val sink = Sink.foreach[String](println(_))
+  val runnable = f.toMat(sink)(Keep.right)
+  val a = runnable.run()
 
-  // connect the Source to the Sink, obtaining a RunnableGraph
-  val runnable: RunnableGraph[Future[Int]] = doubleSource.toMat(sink)(Keep.right)
-
-  // materialize the flow and get the value of the FoldSink
-  val sum: Future[Int] = runnable.run()
-
-  sum.flatMap(x => Future { println(x) })
+  //  val source = Source(1 to 10)
+//  val doubles = (x: Int) => x * 2
+//  val doubleSource = source.map(doubles)
+//  val sink = Sink.fold[Int, Int](0)(_ + _)
+//
+//  // connect the Source to the Sink, obtaining a RunnableGraph
+//  val runnable: RunnableGraph[Future[Int]] = doubleSource.toMat(sink)(Keep.right)
+//
+//  // materialize the flow and get the value of the FoldSink
+//  val sum: Future[Int] = runnable.run()
+//
+//  sum.flatMap(x => Future { println(x) })
 }
